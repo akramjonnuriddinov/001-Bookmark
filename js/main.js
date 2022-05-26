96f9fd5c466a6a1e93f01e0fcd3278a99f016cef
akramjonnuriddinov <akramjonmohirdev@gmail.com>
2022-05-26 11:40:56 +0500

    Add accardion, contact section and styles

--- js/main.js
+++ js/main.js
@@ -1,47 +1,32 @@
-// const elCarouselBtn = document.querySelectorAll('.js-carousel__btn');
-// const elCarouselLine = document.querySelectorAll('.js-carousel__line');
+// -------------------------CAROUSEL VARIABLE-------------------------
+const elCarouselBtn = document.querySelectorAll('.js-carousel__btn');
+const elCarouselLine = document.querySelectorAll('.js-carousel__line');
 
-// for (let i = 0; i < elCarouselLine.length; i++) {
-//     // for (let k = 0; k < elCarouselLine.length; k++) {
-//     //     elCarouselLine[k].classList.remove('carousel__line--show');
-//     // }
-//     elCarouselBtn[i].addEventListener('click', function() {
+// SHOW CAROUSEL LINE
+elCarouselBtn.forEach(function(item, index) {
+    item.addEventListener('click', function() {
+        elCarouselLine.forEach(function(item) {
+            item.classList.remove('carousel__line--show');
+        });
 
-//         elCarouselLine[i].classList.add('carousel__line--show');
-//     });
-// }
-
-
-// elCarouselBtn.forEach(function (item, index) {
-//     item.addEventListener('click', function () {
-//         elCarouselLine.classList.add('carousel__line--show');
-//         elCarouselLine.classList.add('carousel__line--show');
-//     });
-// });
+        elCarouselLine[index].classList.add('carousel__line--show');
+    });
+});
 
 
-
+// -------------------------ACCORDION VARIABLE-------------------------
 const elAccordionBtn = document.querySelectorAll('.js-accordion-btn');
-const elAccordionText = document.querySelectorAll('.js-accordion-text');
+const elAccordionText = document.querySelectorAll('.js-accordion-text-wrapper');
+const elAccordionIcon = document.querySelectorAll('.js-accordion-icon')
 
+// SHOW ACCORDION TEXT
 elAccordionBtn.forEach(function(item, index) {
     item.addEventListener('click', function() {
-        elAccordionText[index].classList.toggle('accordion__text--show');
+        elAccordionText.forEach(function (item){
+            item.classList.remove('accordion__text-wrapper--show');
+        });
+        
+        elAccordionText[index].classList.toggle('accordion__text-wrapper--show');
+        elAccordionIcon[index].classList.toggle('accordion__icon--rotate');
     });
-})
-
-// har bir button bosilganda
-    // har bir line dan faollikni olib tashlaymiz
-    // bosilgan buttondan keyingi elementga show klassini qo'shamiz - .nextElementSiblinh
-
-    
-// 1. 3 ta button va har birini tagida chiziq bor.
-// 2. Buttonlar click bo'lganda, pastdagi chiziq orqa foni transparentdan orange ranga o'tishi kerak yoki display yo'q  holatdan display bor holatga o'tishi kerak.
-// 3. Agar bitta button click bo'lib orqasidan 2-button ham click bo'lsa, birinchi buttondagi backgroundni transparentga o'tkazish kerak.
-//     [1] [2] [3]
-
-//     [3] - > arr.length - 1, arr.length - 2
-//     [2] - > arr.length - 1, arr.length + 1
-//     [1] - > arr.length + 1, arr.length + 2
-
-//     Button indexi bilan carousel indexi teng bo'lsa class qo'shilsin teng bo'lmaganidan classini remove qilib qo'yilsin. 
+});
